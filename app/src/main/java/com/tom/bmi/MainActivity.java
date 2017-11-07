8c99e1bd853289ab3356dcbb9d103a5c1b6cfdd1
hm5829 <hm5829@yahoo.com.tw>
2017-11-07 10:56:00 +0800

    initial commit

--- app/src/main/java/com/tom/bmi/MainActivity.java
+++ app/src/main/java/com/tom/bmi/MainActivity.java
@@ -5,15 +5,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // getResources().getString(R.string.app_name);
+        // 上面 = "Bmi"
+        Button bInfo = (Button) findViewById(R.id.b_info);
+        bInfo.setOnClickListener(listener);
     }
     public void bmi(View view){
      //   System.out.println();
@@ -24,9 +34,10 @@ public class MainActivity extends AppCompatActivity {
         float height = Float.parseFloat(edHeight.getText().toString());
         float bmi = weight / (height * height);
         new AlertDialog.Builder(this)
-                .setMessage("Your BMI is " + bmi)
-                .setTitle("my title")
-                .setPositiveButton("OK", null)
+                .setMessage(getString(R.string.your_bmi_is) + bmi)
+                // = .setMessage("Your BMI is " + bmi)
+                .setTitle(R.string.bmi_title)
+                .setPositiveButton(R.string.ok, null)
                 .show();
         /*Log.d("MainActivity", "You Bmi is : " +bmi);
         Toast.makeText(this, "Your BMI is " + bmi, Toast.LENGTH_LONG).show();*/
